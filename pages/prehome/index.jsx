201179d4cf88ed17b14c8b244cf28b9a53098413
a0p0dp0 <armando.paulo0@walmart.com>
2023-10-09 22:54:14 -0300

    feat: added initials pre-home page

---
+++ pages/prehome/index.jsx
@@ -0,0 +1,11 @@
+import styles from './styles.module.css'
+
+const PreHome = () => {
+    return (
+        <div className={styles.container}>
+            <h1>PRE-HOME PAGE (NextJs)</h1>
+        </div>
+    )
+}
+
+export default PreHome
